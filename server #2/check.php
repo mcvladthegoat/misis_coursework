ba608bd0b5681df59cb26e9322f0b79236a1cff6
Vlad Levitskiy <swimer96@mail.ru>
2015-04-08 17:52:47 +0300

    Исправлен check.php

--- server #2/check.php
+++ server #2/check.php
@@ -5,7 +5,7 @@ if(file_exists("files/".$_GET["id"].".json")){
 	fclose($link);
 	$d = json_decode($input);
 	$con = new mysqli("localhost", "mcvlad", "thealgoritm2012", "mcvlad");
-	$con->query("INSERT INTO board VALUES('$d[0]', '$d[1]', '$d[2]', '$d[2]')");
+	$con->query("INSERT INTO stock VALUES('$d[0]', '$d[1]', '$d[2]', '$d[3]')");
 	unlink("files/".$_GET["id"].".json");
 	echo "OK";
 }
