186586b0edc9b44db2a3e55cd2c7e52872ee330b
mcvladthegoat <cross-tester@inbox.ru>
2015-04-08 17:50:19 +0300

    Случайно забыл про 2й сервер :)

---
+++ server #2/check.php
@@ -0,0 +1,13 @@
+<?php
+if(file_exists("files/".$_GET["id"].".json")){
+	$link = fopen("files/".$_GET["id"].".json", "r");
+	$input = fgets($link);
+	fclose($link);
+	$d = json_decode($input);
+	$con = new mysqli("localhost", "mcvlad", "thealgoritm2012", "mcvlad");
+	$con->query("INSERT INTO board VALUES('$d[0]', '$d[1]', '$d[2]', '$d[2]')");
+	unlink("files/".$_GET["id"].".json");
+	echo "OK";
+}
+else{echo "error";}
+?>
